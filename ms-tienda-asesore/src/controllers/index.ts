8d5f93e6f669bae997482160262223af54f091da
Khadija1103 <tatianamarrugo1103@gmail.com>
2021-11-19 08:27:11 -0500

    contrladores de asesor y cliente

--- ms-tienda-asesore/src/controllers/index.ts
+++ ms-tienda-asesore/src/controllers/index.ts
@@ -2,3 +2,5 @@ export * from './ping.controller';
 export * from './cliente-asesor.controller';
 export * from './asesor-cliente.controller';
 export * from './asesor-usuario-asesor.controller';
+export * from './cliente.controller';
+export * from './asesor.controller';
